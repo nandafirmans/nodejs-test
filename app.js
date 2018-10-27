95fb5e55d39d09dd243e1f35eb0ccb471df69fae
nandafirmans <nandafirmans@gmail.com>
2018-10-28 05:07:54 +0700

    adding node Joi for validating request

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 
 const Courses = require("./courses");
-const courses = new Courses();
-
+const Joi = require("joi");
 const express = require("express");
+const courses = new Courses();
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
 
 app.get("/api/courses", (req, res) => {
     res.send(courses.fetch());
@@ -18,17 +14,33 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
     const course = courses.fetch(req.params.id);
 
-    if (!course)
+    if (!course) {
         res.status("404").send("courses with given ID is not exist");
+        return;
+    }
 
     res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
-    const newCourse = courses.insert(req.body.name);
+    const schema = {
+        name : Joi.string().min(3).required()
+    };
 
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) {
+        const errMessage = result.error.details.map(err => err.message).join(" | ");
+
+        res.status(400).send(errMessage);
+        return;
+    }
+
+    const newCourse = courses.insert(req.body.name);
     res.send(newCourse);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
